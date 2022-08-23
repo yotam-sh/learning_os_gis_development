721530bcb36f2a39dd41fa9a5494b52ec2c29fd8
Yotam <yotamshavit@gmail.com>
2022-08-23 11:46:32 +0300

    added csv identifying for specific csv created for this first day lesson

--- day_1_learning_pandas.py
+++ day_1_learning_pandas.py
@@ -1,6 +1,12 @@
 import os
-import numpy
+# import numpy
 # import pandas
 
+# Change workspace
 os.chdir(r'C:\Users\user\Desktop\PyForFun\geopandas\day_1_learning_pandas')
+file_list = os.listdir(os.getcwd())
 print(f'Current working directory: {os.getcwd()}')
+print(f'File list:{file_list}')
+if len(file_list) == 1 and file_list[0].endswith('.csv'):
+    f_path = f'{os.getcwd()}\\{file_list[0]}'
+
