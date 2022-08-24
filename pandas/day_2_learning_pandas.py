ee5bf4b998903475f750432d8c2d29fe7d77a958
Yotam <yotamshavit@gmail.com>
2022-08-24 10:56:04 +0300

    import pandas and create a table from dict

--- pandas/day_2_learning_pandas.py
+++ pandas/day_2_learning_pandas.py
@@ -1,8 +1,13 @@
 # day 2
 
+import pandas as pd
+
 classmates = {
     'first_name': ['Yotam', 'Shaked', 'Idan', 'Maia'],
     'last_name': ['Shavit', 'Yefet', 'Ben-Atar', 'Abeles'],
     'age': [26, 28, 27, 23],
     'profession': ['GIS Professional', 'GIS Analyst', 'GIS Analyst', 'Bartender']
 }
+
+df = pd.DataFrame(classmates)
+print(df)
