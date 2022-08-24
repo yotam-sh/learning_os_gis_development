a7d2212a53a0721954cb1a48aca833c6844a1b64
Yotam <yotamshavit@gmail.com>
2022-08-24 10:54:35 +0300

    created dict to manipulate

--- pandas/day_2_learning_pandas.py
+++ pandas/day_2_learning_pandas.py
@@ -1,1 +1,8 @@
 # day 2
+
+classmates = {
+    'first_name': ['Yotam', 'Shaked', 'Idan', 'Maia'],
+    'last_name': ['Shavit', 'Yefet', 'Ben-Atar', 'Abeles'],
+    'age': [26, 28, 27, 23],
+    'profession': ['GIS Professional', 'GIS Analyst', 'GIS Analyst', 'Bartender']
+}
