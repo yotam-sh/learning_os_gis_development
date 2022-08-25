858549cd5bd03c156a7e2bf03871f3091d57df0a
Yotam <yotamshavit@gmail.com>
2022-08-25 11:17:59 +0300

    set index

--- pandas/day_3_learning_pandas.py
+++ pandas/day_3_learning_pandas.py
@@ -2,3 +2,16 @@
 
 import numpy as np
 import pandas as pd
+
+classmates = {
+    'first_name': ['Yotam', 'Shaked', 'Idan', 'Maya'],
+    'last_name': ['Shavit', 'Yefet', 'Ben-Atar', 'Abeles'],
+    'age': [26, 28, 27, 23],
+    'sex': ['Male', 'Male', 'Male', 'Female'],
+    'profession': ['GIS Professional', 'GIS Analyst', 'GIS Analyst', 'Bartender']
+}
+
+df = pd.DataFrame(classmates)
+df.set_index(['first_name', 'last_name'], inplace=True)
+
+print(df.loc['Yotam'])
