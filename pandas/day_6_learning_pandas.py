7f2684953ee9c8a5992169f0ad4207a8a84227de
yotam-sh <yotamshavit@gmail.com>
2022-08-31 11:12:55 +0300

    new base structure for working with these pandas examples

---
+++ pandas/day_6_learning_pandas.py
@@ -0,0 +1,33 @@
+# Day 6
+
+import os
+import numpy as np
+import pandas as pd
+
+##
+
+def init_work():
+    os.chdir(r'C:\Users\yotam\Documents\Python\material\for_future_lessons\2019survey_copy')
+    file_list = os.listdir(os.getcwd())
+    return file_list
+
+def df_creator(work_csv, schema_csv):
+    df = pd.read_csv(wcsv_path, index_col='Respondent')
+    schema_df = pd.read_csv(scsv_path, index_col='Column').sort_index() # Sort by index 'Column'
+    return df, schema_df
+
+def check_csv(file_list):
+    assert "survey_results_public.csv" == file_list[2], "work csv not found"
+    work_csv = file_list[2]
+    wcsv_path = os.path.normpath("\\".join([os.getcwd(), work_csv])) # set work csv path
+
+    assert "survey_results_schema.csv" == file_list[3], "schema csv not found"
+    schema_csv = file_list[3]
+    scsv_path = os.path.normpath("\\".join([os.getcwd(), schema_csv])) # set schema file path
+
+    return df_creator(wcsv_path, scsv_path)
+
+def csv_manip(dataframe):
+    something
+    
+df, schema_df = check_csv(init_work())
