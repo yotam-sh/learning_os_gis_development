950ca11185adcec7a5bfb55d6d62b91ecacf939b
yotam-sh <yotamshavit@gmail.com>
2022-09-06 16:13:04 +0300

    started work on load data function

--- pandas/day_9_project-final.py
+++ pandas/day_9_project-final.py
@@ -4,3 +4,16 @@ import pandas as pd
 from datetime import datetime
 
 ##
+
+def load_data():
+    # Set workspace and get list of files
+    os.chdir(r'C:\Users\yotam\Documents\Python\material\for_future_lessons\stack-overflow-developer-survey-2022')
+    workdir_files = os.listdir(os.getcwd())
+
+    # Set work and schema csv names
+    main_csv = 'survey_results_public.csv'
+    schema_csv = 'survey_results_schema.csv'
+
+    print(workdir_files)
+
+load_data()
