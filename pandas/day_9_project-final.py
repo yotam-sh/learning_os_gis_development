9bb9e415a779467a86e8f55a0f7c8c64f0bebb23
yotam-sh <yotamshavit@gmail.com>
2022-09-07 12:30:20 +0300

    Replace Education values with shorter vals

--- pandas/day_9_project-final.py
+++ pandas/day_9_project-final.py
@@ -30,17 +30,26 @@ def work_with_data(df):
     nis_rate = 3.42
     df['YearlyCompNIS'] = [i * nis_rate for i in df.ConvertedCompYearly]
 
+    # Replace EdLevel values with shorter values
+    ed_levels = ["Bachelor’s degree (B.A., B.S., B.Eng., etc.)",
+                 "Master’s degree (M.A., M.S., M.Eng., MBA, etc.)",
+                 "Associate degree (A.A., A.S., etc.)",
+                 "Other doctoral degree (Ph.D., Ed.D., etc.)",
+                 "Professional degree (JD, MD, etc.)"]
+    
+    df.loc[~df["EdLevel"].isin(ed_levels), "EdLevel"] = "Non-Academic"
+    df['EdLevel'].replace(to_replace=[ed_levels[0], ed_levels[1], ed_levels[2], ed_levels[3], ed_levels[4]], 
+                          value=["Bachelor's", "Master's", "Associate", "Doctoral", "Professional"], inplace=True)
+
     # Filter
     filt = (df['Country'] == 'Israel') & (df['ConvertedCompYearly'] > 0)
-    relevant_cols = ['YearsCode', 'YearsCodePro', 'EdLevel', 'YearlyCompNIS']
-
+    relevant_cols = ['EdLevel', 'YearsCode', 'YearsCodePro', 'WorkExp', 'YearlyCompNIS', 'Age', 'Gender', 'Sexuality', 'Accessibility', 'MentalHealth']
     filter_df = df[filt][relevant_cols]
 
-    print(filter_df.head())
-
-    # return filter_df
+    return filter_df
 
 ## 
 
 df = load_data()
 df = work_with_data(df)
+print(df.value_counts(['EdLevel']))
