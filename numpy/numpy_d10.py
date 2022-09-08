d96fb07436f114abce6c371b3f6dbc30d97b2dd9
yotam-sh <yotamshavit@gmail.com>
2022-09-08 11:48:06 +0300

    basic commands

--- numpy/numpy_d10.py
+++ numpy/numpy_d10.py
@@ -6,5 +6,22 @@ import numpy as np
 
 #
 
-a = np.array([1, 2, 3])
-print(a)
+a = np.array([1, 2, 3], dtype='int16')
+b= np.array([[9.0, 8.0, 5.0], [6.0, 5.0, 4.0]])
+
+# Get dimension
+print(f'\nDimension: {b.ndim}') # --> 2 because it's two lists
+
+# Get shape
+print(f'Shape: {b.shape}') # --> (2, 3) because it's 2 lists containing 3 values (size 3)
+
+# Get type
+print(f'Array "a" type: {a.dtype}') # --> int16
+print(f'Array "b" type: {b.dtype}') # --> float64
+
+# Get size
+print(f'Array "a" size: {a.itemsize}')
+
+# Get total size
+print(f'Total size array "a": {a.size * a.itemsize}')
+print(f'Total size array "a": {a.nbytes}') # --> array.nbytes == array.size * array.itemsize
